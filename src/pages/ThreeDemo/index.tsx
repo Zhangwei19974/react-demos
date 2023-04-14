05a0f7fe4e5ddffac0190d9c12e5906b05974adc
张伟 <zhangw@tongfun.com>
2023-04-14 19:10:55 +0800

    add:新增threejs的练习代码

--- src/pages/ThreeDemo/index.tsx
+++ src/pages/ThreeDemo/index.tsx
@@ -4,19 +4,47 @@ import ContainerFlex from '../../components/containers/container-flex';
 import {Button, Card} from 'antd';
 import {isSupportWebgl} from '@/utils/env-check';
 import {
+  AmbientLight,
   AxesHelper,
-  BoxGeometry, BufferGeometry, Camera, Line, LineBasicMaterial,
+  BoxGeometry,
+  BufferAttribute,
+  BufferGeometry,
+  Camera, CatmullRomCurve3,
+  DirectionalLight,
+  DoubleSide,
+  GridHelper,
+  Group, LatheGeometry,
+  Line,
+  LineBasicMaterial, LineLoop,
   Material,
   Mesh,
-  MeshBasicMaterial, MeshLambertMaterial, MeshPhongMaterial, Object3D,
-  PerspectiveCamera, PointLight, Renderer,
-  Scene, Vector3,
-  WebGLRenderer
+  MeshBasicMaterial,
+  MeshLambertMaterial,
+  MeshPhongMaterial,
+  Object3D,
+  PerspectiveCamera,
+  PlaneGeometry,
+  PointLight,
+  Points,
+  PointsMaterial,
+  Renderer,
+  RepeatWrapping,
+  Scene, Shape,
+  ShapeGeometry,
+  TextureLoader, TubeGeometry, Vector2,
+  Vector3,
+  WebGLRenderer,
+
 } from 'three';
 import {MaterialParameters} from 'three/src/materials/Material';
 import {TextGeometry} from 'three/examples/jsm/geometries/TextGeometry';
 import TWEEN from '@tweenjs/tween.js';
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
+import Stats from 'three/examples/jsm/libs/stats.module';
+import {ParametricGeometries} from 'three/examples/jsm/geometries/ParametricGeometries';
+import SphereGeometry = ParametricGeometries.SphereGeometry;
+import {GUI} from 'dat.gui';
+import yige from '@/assets/yige.png'
 
 function Index() {
   // const isWebGl = isSupportWebgl()
@@ -27,7 +55,7 @@ function Index() {
     // 创建相机
     const width = 800
     const height = 500
-    let camera:Camera
+    let camera:PerspectiveCamera
     // 创建渲染器
     let renderer:Renderer
     let callBackFun:(()=>void) | undefined
@@ -39,12 +67,15 @@ function Index() {
     }
     function init(){
       scene = new Scene()
-      camera = new PerspectiveCamera(30, width/height, 1, 3000)
+      camera = new PerspectiveCamera(30, width/height, 1, 2000)
       camera.position.set(400, 400, 400)
       camera.lookAt(0, 0, 0)
 
       const axesHelper = new AxesHelper(150)
       scene.add(axesHelper)
+
+      const  gridHelper = new GridHelper(400, 25, 0x004444, 0x004444)
+      scene.add(gridHelper)
       keepAliveObj.push(axesHelper)
       keepAliveObj.push(scene)
       renderer = new WebGLRenderer()
@@ -66,10 +97,11 @@ function Index() {
       return{
         scene,
         camera,
-        renderer
+        renderer,
+        dom
       }
     }
-    function setCamera(cameraInfo:Camera){
+    function setCamera(cameraInfo:PerspectiveCamera){
       camera = cameraInfo
     }
     function setScene(sceneInfo:Scene){
@@ -97,14 +129,16 @@ function Index() {
     }
   }
   const {init, remove, setAnimation, keepAliveObj, setCamera, setRenderer, setScene } = firstThree()
-  let cameraInfo:Camera
+  let cameraInfo:PerspectiveCamera
   let sceneInfo:Scene
   let rendererInfo:Renderer
+  let domInfo:HTMLElement
   useEffect(()=>{
-    const {camera, scene, renderer} = init()
+    const {camera, scene, renderer, dom} = init()
     cameraInfo = camera
     sceneInfo = scene
     rendererInfo = renderer
+    domInfo = dom
     return()=>{
       remove()
     }
@@ -140,8 +174,8 @@ function Index() {
     sceneInfo.add(mesh)
 
     setAnimation(()=>{
-      mesh.rotation.x += 0.01
-      mesh.rotation.y += 0.01
+      mesh.rotation.x += Math.PI/20
+      mesh.rotation.y += Math.PI/20
     })
   }
 
@@ -194,7 +228,7 @@ function Index() {
     mesh1.position.set(0, 0, 0)
     const mesh2 = new Mesh(box, material2)
     mesh2.position.set(200, 200, 200)
-    const pointLight = new PointLight('#fff', 1.0)
+    const pointLight = new PointLight('#fff', 10)
     pointLight.position.set(400, 0, 0)
     sceneInfo.add(mesh1)
     sceneInfo.add(mesh2)
@@ -207,9 +241,337 @@ function Index() {
       rendererInfo.render(sceneInfo, cameraInfo)
     })
   }
+  function fullScreen(){
+    rendererInfo.setSize(window.innerWidth, window.innerHeight)
+    cameraInfo.aspect = window.innerWidth/ window.innerHeight
+    cameraInfo.updateProjectionMatrix()
+  }
+  function installStats(){
+    const stats = new Stats()
+    console.log(stats)
+    stats.dom.setAttribute('position', 'relative')
+    domInfo.appendChild(stats.dom)
+    setAnimation(()=>{
+      stats.update()
+    })
+  }
+  function cubeArray(){
+    const geometry = new BoxGeometry(100, 100, 100)
+    const material = new MeshLambertMaterial({
+      transparent: true,
+      opacity: 0.5,
+      color: 0x00ffff,
+
+    })
+    const point = new PointLight('#ffffff', 10)
+    point.position.set(800, 0, 800)
+    sceneInfo.add(point)
+    for (let i=0;i<10;i++){
+      for (let j=0;j<10;j++){
+        const mash = new Mesh(geometry, material)
+        mash.position.set(200*i, 0, 200*j)
+        setAnimation(()=>{
+          mash.rotation.x += 0.1
+          mash.rotation.y += 0.1
+        })
+        sceneInfo.add(mash)
+      }
+    }
+  }
+
+  function createPhongMaterial(){
+    const point = new PointLight(0xfff, 10)
+    point.position.set(400, 0, 400)
+    sceneInfo.add(point)
+    const geometry = new SphereGeometry(50, 1000, 100)
+    const material = new MeshPhongMaterial({
+      color: 0x00ffff
+    })
+    const mesh = new Mesh(geometry, material)
+    sceneInfo.add(mesh)
+  }
+  function createGui(){
+    const gui = new GUI()
+    gui.domElement.style.right = '0px'
+    gui.domElement.style.width = '300px'
+    gui.domElement.style.position = 'absolute'
+    gui.domElement.style.top = '0px'
+    gui.domElement.style.right = '0px'
+    const obj = {
+      x: 30
+    }
+    gui.add(obj, 'x', 0, 100)
+    console.log(gui)
+    domInfo.appendChild(gui.domElement)
+  }
+  function createGeometryAndPoint(){
+    const geometry = new BufferGeometry()
+    const vertices = new Float32Array([
+      0, 0, 0, //顶点1坐标
+      50, 0, 0, //顶点2坐标
+      0, 100, 0, //顶点3坐标
+      0, 0, 10, //顶点4坐标
+      0, 0, 100, //顶点5坐标
+      50, 0, 10, //顶点6坐标
+    ])
+    const attrubue = new BufferAttribute(vertices, 3)
+    geometry.setAttribute('position', attrubue)
+    const material= new PointsMaterial({
+      color: 0xffff00,
+      size: 10.0 //点对象像素尺寸
+    })
+    const points = new Points(geometry, material)
+    sceneInfo.add(points)
+  }
+  function createLineModel(){
+    const geometry = new BufferGeometry()
+    const vertices = new Float32Array([
+      0, 0, 0, //顶点1坐标
+      50, 0, 0, //顶点2坐标
+      0, 100, 0, //顶点3坐标
+      0, 0, 10, //顶点4坐标
+      0, 0, 100, //顶点5坐标
+      50, 0, 10, //顶点6坐标
+    ])
+    const attrubue = new BufferAttribute(vertices, 3)
+    geometry.setAttribute('position', attrubue)
+    const material = new LineBasicMaterial({
+      color: 0xff0000
+    })
+    const line = new Line(geometry, material)
+    sceneInfo.add(line)
+  }
+  function createSquare(){
+    const geometry = new BufferGeometry()
+    const vertices = new Float32Array([
+      0, 0, 0, //顶点1坐标
+      80, 0, 0, //顶点2坐标
+      80, 80, 0, //顶点3坐标
+
+      0, 0, 0, //顶点4坐标   和顶点1位置相同
+      80, 80, 0, //顶点5坐标  和顶点3位置相同
+      0, 80, 0, //顶点6坐标
+    ])
+    const attribute = new BufferAttribute(vertices, 3)
+    geometry.setAttribute('position', attribute)
+    const material = new MeshBasicMaterial({
+      color: '#770000',
+      side: DoubleSide
+    })
+    const mesh = new Mesh(geometry, material)
+    sceneInfo.add(mesh)
+  }
+  function createSquareWithPointIndex(){
+    const geometry = new BufferGeometry()
+    const vertices = new Float32Array([
+      0, 0, 0, //顶点1坐标
+      80, 0, 0, //顶点2坐标
+      80, 80, 0, //顶点3坐标
+      0, 80, 0, //顶点4坐标
+    ])
+
+    const index = new Uint16Array([0, 1, 2, 0, 2, 3,])
+    geometry.index = new BufferAttribute(index, 1)
+    geometry.setAttribute('position', new BufferAttribute(vertices, 3) )
+    const material = new MeshBasicMaterial({
+      color: '#770000',
+      side: DoubleSide
+    })
+    const mesh = new Mesh(geometry, material)
+    sceneInfo.add(mesh)
+  }
+  function createGroup(){
+    const geometry = new BoxGeometry(20, 20, 20)
+    const  material = new MeshPhongMaterial({
+      opacity: 0.5,
+      color: 0xff0000,
+      transparent: true
+    })
+    const group = new Group()
+    const mesh = new Mesh(geometry, material)
+    const mesh2 = mesh.clone()
+    const point = new PointLight('#fff', 10)
+    mesh2.position.x = 40
+    group.add(mesh)
+    group.add(mesh2)
+    sceneInfo.add(point)
+    sceneInfo.add(group)
+    point.position.set(80, 80, 80)
+    setAnimation(()=>{
+      group.rotation.x += 0.03
+      group.rotation.y += 0.03
+      group.rotation.z += 0.03
+    })
+  }
+  function createTexture(){
+    const geometry = new SphereGeometry(50, 100, 100)
+    const textureLoader = new TextureLoader()
+    const texture = textureLoader.load(yige)
+    const material = new MeshBasicMaterial({
+      color: 0xffffff,
+      map: texture
+    })
+    const mesh = new Mesh(geometry, material)
+    const point = new PointLight('#fff', 10)
+    point.position.set(80, 1, 3)
+    console.log(mesh)
+    sceneInfo.add(mesh)
+    sceneInfo.add(point)
+
+  }
+  function createTextureWrap(){
+    const geometry = new PlaneGeometry(200, 200)
+    const textureLoader = new TextureLoader()
+    const texture = textureLoader.load(yige)
+    texture.wrapT = RepeatWrapping
+    texture.wrapS = RepeatWrapping
+    texture.repeat.set(2, 2)
+    const material = new MeshBasicMaterial({
+      color: 0xffffff,
+      map: texture
+    })
+    const mesh = new Mesh(geometry, material)
+    const point = new PointLight('#fff', 10)
+    point.position.set(80, 1, 3)
+    console.log(mesh)
+    sceneInfo.add(mesh)
+    sceneInfo.add(point)
+    setAnimation(()=>{
+      texture.offset.x += 0.01
+    })
+  }
+  function create(){
+    const light = new DirectionalLight(0xffffff, 2)
+    sceneInfo.add(light)
+  }
+  function createArc(){
+    const geometry = new BufferGeometry()
+    const  R = 100
+    const N = 50
+    const sp = Math.PI*2/N
+    const  arr  = []
+    for (let i = 0; i<N; i++){
+      const angle = sp * i
+      const x = R * Math.cos(angle)
+      const y = R * Math.sin(angle)
+      arr.push(x, y, 0)
+    }
+    const  vertices = new Float32Array(arr)
+    const attribue  = new BufferAttribute(vertices, 3)
+    geometry.setAttribute('position', attribue)
+
+    const material = new LineBasicMaterial({
+      color: 0xff0000
+    })
+    const line= new  LineLoop(geometry, material)
+    sceneInfo.add(line)
+
+  }
+  function createCatmullRomCurve3(){
+    const arr = [
+      new Vector3(-50, 20, 90),
+      new Vector3(-10, 40, 40),
+      new Vector3(0, 0, 0),
+      new Vector3(60, -60, 0),
+      new Vector3(70, 0, 80)
+    ]
+    const curve = new CatmullRomCurve3(arr)
+    const points = curve.getPoints(100)
+    const geometry = new BufferGeometry()
+    geometry.setFromPoints(points)
+    const material  = new LineBasicMaterial({
+      color: 0xff0000
+    })
+    const  line= new Line(geometry, material)
+    sceneInfo.add(line)
+  }
+  function createTube(){
+    const  path = new CatmullRomCurve3([
+      new Vector3(-50, 20, 90),
+      new Vector3(-10, 40, 40),
+      new Vector3(0, 0, 0),
+      new Vector3(60, -60, 0),
+      new Vector3(70, 0, 80)
+    ])
+    const geometry = new TubeGeometry(path, 40, 2, 25)
+    const material = new MeshBasicMaterial({
+      color: 0xff0000,
+      side: DoubleSide, //双面显示看到管道内壁
+    });
+    const mesh = new Mesh(geometry, material)
+    sceneInfo.add(mesh)
+  }
+  function createLathGeometry(){
+    const  path = [
+      new Vector2(-50, 20),
+      new Vector2(-10, 40),
+      new Vector2(0, 0),
+      new Vector2(60, -60),
+      new Vector2(70, 0)
+    ]
+    const geometry = new LatheGeometry(path)
+    const material = new MeshBasicMaterial({
+      color: 0xff0000,
+      side: DoubleSide, //双面显示看到管道内壁
+    });
+    const mesh = new Mesh(geometry, material)
+    sceneInfo.add(mesh)
+  }
+  function createPolygonContourArc(){
+    const shape = new Shape()
+    shape.lineTo(100+50, 0)
+    shape.arc(-50, 0, 50, 0, Math.PI/2, false)
+    shape.lineTo(0, 50)
+    const geometry = new ShapeGeometry(shape)
+    const material = new MeshBasicMaterial({
+      color: 0xff0000,
+      side: DoubleSide, //双面显示看到管道内壁
+    });
+    const mesh = new Mesh(geometry, material)
+    sceneInfo.add(mesh)
+  }
+  function createTubeWatch(){
+    const path = new CatmullRomCurve3([
+      new Vector3(-50, 20, 90),
+      new Vector3(-10, 40, 40),
+      new Vector3(0, 0, 0),
+      new Vector3(60, -60, 0),
+      new Vector3(90, -40, 60),
+      new Vector3(120, 30, 30),
+    ])
+    const geometry = new TubeGeometry(path, 100, 5, 30)
+    const textureLoader = new TextureLoader()
+    const texture = textureLoader.load(yige)
+    texture.wrapS = RepeatWrapping
+
+    const material = new MeshBasicMaterial({
+      map: texture,
+      side: DoubleSide, //双面显示看到管道内壁
+    });
+    const mesh = new Mesh(geometry, material)
+    let i = 0
+    const pointArr = path.getPoints(1000)
+    const tween = new TWEEN.Tween(cameraInfo.position)
+    tween.to(pointArr[i], 1000)
+    tween.onUpdate(()=>{
+      cameraInfo.lookAt(pointArr[1])
+    })
+    tween.start()
+    tween.onComplete(()=>{
+      setAnimation(()=>{
+        if(i+1 <= pointArr.length){
+          console.log('Asdasd')
+          cameraInfo.position.copy(pointArr[i])
+          cameraInfo.lookAt(pointArr[i+1])
+          i+=1
+        }
+      })
+    })
+    sceneInfo.add(mesh)
+  }
   return (
     <ContainerFlex>
-      <div id={'three'} />
+      <div id={'three'} style={{position: 'relative'}} />
       {/*<Card style={{width: 300, height: 600}}>*/}
       {/*  asdasd*/}
       {/*</Card>*/}
@@ -221,6 +583,25 @@ function Index() {
         <Button onClick={()=>changeCameraPosition()}>切换camera位置</Button>
         <Button onClick={()=>createLightObject()}>创建有光源的场景</Button>
         <Button onClick={()=>createOrbitControls()}>创建相机组件</Button>
+        <Button onClick={()=>fullScreen()}>全屏</Button>
+        <Button onClick={()=>installStats()}>引入Stats</Button>
+        <Button onClick={()=>cubeArray()}>阵列立方体和相机适配体验</Button>
+        <Button onClick={()=>createPhongMaterial()}>创建高光材质</Button>
+        <Button onClick={()=>createGui()}>创建GUI</Button>
+        <Button onClick={()=>createGeometryAndPoint()} >创建点模型</Button>
+        <Button onClick={()=>createLineModel()}>创建线模型</Button>
+        <Button onClick={()=>createSquare()}>三角形拼接面</Button>
+        <Button onClick={()=>createSquareWithPointIndex()}>使用顶点索引数据创建三角形拼接的正方体</Button>
+        <Button onClick={()=>createGroup()}>分组</Button>
+        <Button onClick={()=>createTexture()}>创建纹理贴图</Button>
+        <Button onClick={()=>createTextureWrap()} >矩形阵列功能</Button>
+        <Button onClick={()=>create()}>添加环境光</Button>
+        <Button onClick={()=>createArc()}>绘制圆弧</Button>
+        <Button onClick={()=>createCatmullRomCurve3()}>三维样条曲线</Button>
+        <Button onClick={()=>createTube()}>曲线路径管道</Button>
+        <Button onClick={()=>createLathGeometry()}>旋转成型</Button>
+        <Button onClick={()=>createPolygonContourArc()}>多边形轮廓圆弧</Button>
+        <Button onClick={()=>createTubeWatch()}>管道漫游</Button>
       </div>
     </ContainerFlex>
   );
